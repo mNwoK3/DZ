834285be3b1554effc386816fbf1cf70ef9b6de1
mnwok <man.nikeman.21@gmail.com>
2024-05-17 11:38:21 +0300

    test try 3

--- app/src/test/java/com/example/dz/UnitTest.kt
+++ app/src/test/java/com/example/dz/UnitTest.kt
@@ -12,7 +12,7 @@ import org.junit.Assert.*
 class UnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(6,2 + 4)
     }
     @Test
     fun doubleToTextLocale_isCorrect() {
