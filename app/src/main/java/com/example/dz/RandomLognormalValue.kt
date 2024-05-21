55c92660e2094ce108c72bbebdd71cda7aa690fb
mnwok <man.nikeman.21@gmail.com>
2024-05-21 12:40:26 +0300

    changed formula

--- app/src/main/java/com/example/dz/RandomLognormalValue.kt
+++ app/src/main/java/com/example/dz/RandomLognormalValue.kt
@@ -3,12 +3,17 @@ package com.example.dz
 
 import java.util.Random
 import kotlin.math.exp
+import kotlin.math.ln
 import kotlin.math.sqrt
-
+import kotlin.math.pow
 class RandomLognormalValue {
     companion object {
-        fun randomLognormalValue(mean: Double, variance: Double): Double {
-            val a: Double = Random().nextGaussian() * sqrt(variance) + mean // latest
+        fun randomLognormalValue(mu: Double, sigmaSq: Double): Double {
+
+            val Mean = ln(mu.pow(2) / sqrt(mu.pow(2) + sigmaSq)) // хз
+            val Dev = sqrt(ln(1 + (sigmaSq / mu.pow(2))))
+
+            val a: Double = Random().nextGaussian() * Dev + Mean
             return exp(a)
         }
 
